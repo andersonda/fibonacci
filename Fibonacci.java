dbe69015d14383aa84e6bd6715d75914d0efcf84
andersonda <andersonda8109@gmail.com>
2023-11-28 16:19:05 -0800

    Added broken fibonacci method

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    /*
+        Computes the nth term of the fibonacci sequence recursively
+     */
+    public static long fib(int n){
+        if(n == 1){
+            return 1;
+        }
+        else{
+            return fib(n-2) + fib(n-1);
+        }
+    }
+}
