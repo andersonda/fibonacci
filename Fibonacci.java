a54015f4aa2d5bda769f59b7123311aa4d59781a
andersonda <andersonda8109@gmail.com>
2023-11-28 16:39:53 -0800

    Added method that prints the fibonacci sequence between a specified start and end point.

--- Fibonacci.java
+++ Fibonacci.java
@@ -11,8 +11,16 @@ public class Fibonacci {
         }
     }
 
+    public static void printSequence(int start, int end){
+        for(int i = start; i <= end; i++){
+            System.out.println(i + " " + fib(i));
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(fib(5));
         System.out.println(fib(10));
+
+        printSequence(1, 8);
     }
 }
